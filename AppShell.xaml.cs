de5814ed65ee2419ab28b692d24239bbb23f5908
Grégoire Chapeaux <gregoire.chapeaux@apexo.net>
2024-12-11 10:16:37 -0500

    Initial commit

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MRE_Sandbox_9
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace MRE_Sandbox_9
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new NewPage1());
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+
+}
